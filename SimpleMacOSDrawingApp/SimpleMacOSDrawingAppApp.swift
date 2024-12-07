b464d5408663b16bbeff430483c605528b916fbf
itsuki <itsuki.enjyoy@gmail.com>
2024-12-07 11:48:33 +0900

    Initial Commit

---
+++ SimpleMacOSDrawingApp/SimpleMacOSDrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMacOSDrawingAppApp.swift
+//  SimpleMacOSDrawingApp
+//
+//  Created by Itsuki on 2024/12/07.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMacOSDrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
